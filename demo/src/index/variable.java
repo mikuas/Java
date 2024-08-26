8f766457ae669a53708608a3d765bb9d5735b0d0
hanser <114514@0721>
2024-08-26 14:59:00 +0800

    .

--- demo/src/index/variable.java
+++ demo/src/index/variable.java
@@ -6,6 +6,12 @@ public class variable {
         double b = 11.45;
 
         System.out.println(a + b);
+
+        int number = 200; // 0000 0000 0000 0000 0000 0000 1100 1000
+        byte bit = (byte) number; // 1100 1000 -> 1011 1000(原码)
+        System.out.println(bit);
+
+
     }
 }
 
