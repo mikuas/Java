5bee8e170d194660b75da26994a5790bc22370a2
Miku <None>
2024-09-17 12:29:22 +0800

    .

---
+++ demo/src/Test/TextTest.java
@@ -0,0 +1,8 @@
+package Test;
+
+public class TextTest {
+    public static void main(String[] args) {
+        System.out.printf("Hello %s", "World\n");
+        System.out.printf("Hello %s", "Java");
+    }
+}
